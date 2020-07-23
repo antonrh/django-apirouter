4103ca80cb6611b5c1ab26ab112d11a75a97f797
Anton Ruhlov <anton@paxful.com>
2020-07-23 15:36:45 +0300

    Add more types

--- apirouter/utils.py
+++ apirouter/utils.py
@@ -1,13 +1,13 @@
 from typing import Callable, Dict
 
-from django.http import HttpRequest, HttpResponseNotAllowed
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
 
 
-def method_dispatch(**method_handlers: Dict[str, Callable]):
-    def invalid_method(request: HttpRequest, *args, **kwargs):  # noqa
+def method_dispatch(**method_handlers: Dict[str, Callable]) -> Callable:
+    def invalid_method(request: HttpRequest, *args, **kwargs) -> HttpResponse:  # noqa
         return HttpResponseNotAllowed(method_handlers.keys())
 
-    def dispatch(request: HttpRequest, *args, **kwargs):
+    def dispatch(request: HttpRequest, *args, **kwargs) -> HttpResponse:
         handler = method_handlers.get(request.method, invalid_method)
         return handler(request, *args, **kwargs)
 
